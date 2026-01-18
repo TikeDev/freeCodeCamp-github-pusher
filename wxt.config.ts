686d5038c3255156682a3db9e3fb0f2a80fe8cf8
TikeDev <12630355+TikeDev@users.noreply.github.com>
2026-01-18 18:59:51 -0500

    Automatically open Chrome with freeCodeCamp URL and Devtools upon pnpm dev

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,11 +2,22 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    permissions: ['storage', 'tabs'],
+  },
   srcDir: 'src',
   webExt: {
     binaries: {
       chrome: '/Applications/Google\ Chrome Beta.app/Contents/MacOS/Google Chrome\ Beta', //  Chrome Beta instead of regular Chrome
       firefox: '/Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox', // Firefox Developer Edition instead of regular Firefox
     },
+    chromiumArgs: [
+      '--user-data-dir=./.wxt/chrome-data', 
+      '--auto-open-devtools-for-tabs',
+      // '--devtools',
+      '--same-tab',
+      'https://www.freecodecamp.org/learn/daily-coding-challenge/archive', 
+
+    ],
   },
 });
